fdcb3e4f3a8486556ade45da1c62da349bab5c86
GAO <gaoxiangshuai@gmail.com>
2018-09-27 01:37:16 -0400

    updated

--- DownloaderGUI.py
+++ DownloaderGUI.py
@@ -1,15 +1,26 @@
 from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QVBoxLayout
 from Downloader import Downloader
+from PyQt5.QtWidgets import QLabel
 
 class Downloader_Main_Window(QMainWindow):
 
-    def __init__(self,  download_manager):
+    def __init__(self,   download_manager=Downloader):
         super().__init__()
         self.setWindowTitle("PyTube Downloader")
         self.resize(800, 600)
         self.__download_manager = download_manager
-        self.__init_layout()
+        self.__ui = self.UI(self)
+        self.setCentralWidget(self.__ui)
 
 
-    def __init_layout(self):
-        pass
+    class UI(QWidget):
+
+        def __init__(self, parent):
+            super().__init__(parent)
+            self.__layout = QVBoxLayout()
+            self.__label = QLabel('Hello World')
+            self.__layout.addWidget(self.__label)
+            self.setLayout(self.__layout)
+
