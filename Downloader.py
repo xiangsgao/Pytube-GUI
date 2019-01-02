17a02bd03f5fe6bfce909db2618ee7db35108ca3
GAO <gaoxiangshuai@gmail.com>
2019-01-01 22:29:29 -0500

    updated

--- Downloader.py
+++ Downloader.py
@@ -9,11 +9,15 @@ class Downloader:
         self.__choice = 0
         self.__mp3_mode = False
         self.__directory = self.__get_default_download_path()
-        self.__vids = None
+        self.__vids = {}
 
     # sets the video to download by its url
-    def set_url(self, url):
+    def add_video(self, url):
         self.__yt = pytube.YouTube(url)
+        video = self.__yt.streams.all()
+        key = video[self.__choice].default_filename
+        self.__vids[key] = video
+        return key
 
     # sets a custom download directory
     def set_download_directory(self, directory):
@@ -25,15 +29,10 @@ class Downloader:
 
     # displays download choices
     def display_download_choices(self):
-        if not self.yt_set():
-            print('need to set url first\n')
-            return
-
-        self.__vids = self.__yt.streams.all()
-        choice_list = [None] * len(self.__vids)
-        for i in range(len(self.__vids)):
-            choice_list[i] = str(print(i, '. ', self.__vids[i]))
-        return list
+        for key, value in self.__vids.items():
+            print("\n" + "------------------------------------------" + key + "------------------------------------------")
+            for i in range(len(value)):
+                print(str(i), '. ', value[i])
 
     # sets the video choices
     def set_video_choice(self, choice):
@@ -43,8 +42,8 @@ class Downloader:
         return self.__directory
 
     # checks if the url is set
-    def yt_set(self):
-        return self.__yt is not None
+    def videos_empty(self):
+        return not any(self.__vids)
 
     # Returns the default downloads path for linux or windows
     def __get_default_download_path(self):
@@ -60,22 +59,21 @@ class Downloader:
 
     # last step, download
     def download(self):
-        if not self.yt_set():
-            print("\nneed to set url")
-            return False
+        if self.videos_empty():
+            print("\nneed to add url")
+            raise Exception("No videos in the list")
+        for key, value in self.__vids.items():
+            value[self.__choice].download(self.__directory)
+            # this converts the mp4 video tp mp3 then delete the mp4 if the mp3 mode is on
+            if self.__mp3_mode is True:
+                downloaded_file = os.path.join(self.__directory, value[self.__choice].default_filename)
+                mp3_file = downloaded_file[:-4] + '.mp3'
+                if os.path.exists(mp3_file):
+                    os.remove(mp3_file)
+                os.rename(downloaded_file, mp3_file)
 
-        self.__vids[self.__choice].download(self.__directory)
-        self.__vids = self.__yt.streams.all()
-        # this converts the mp4 video tp mp3 then delete the mp4 if the mp3 mode is on
-        if self.__mp3_mode is True:
-            downloaded_file = os.path.join(self.__directory, self.__vids[self.__choice].default_filename)
-            mp3_file = downloaded_file[:-4] + '.mp3'
-            print(downloaded_file)
-            print(mp3_file)
-            os.rename(downloaded_file, mp3_file)
-            # below is for removing the original mp4, doesn't need becuase we are just gonna rename the file to mp3 instead of actually converting
-            # subprocess.run(['rm', os.path.join(self.__directory, downloaded_file)])
-        return True
+                # below is for removing the original mp4, doesn't need becuase we are just gonna rename the file to mp3 instead of actually converting
+                # subprocess.run(['rm', os.path.join(self.__directory, downloaded_file)])
 
 
 
@@ -83,8 +81,9 @@ class Downloader:
 
 if __name__ == '__main__':
     downloader = Downloader()
-    downloader.set_url('https://www.youtube.com/watch?v=gqJG2QFbgfg')
+    downloader.add_video('https://www.youtube.com/watch?v=gqJG2QFbgfg')
+    downloader.add_video('https://www.youtube.com/watch?v=2YRAJt-LbkM')
     downloader.mp3_mode_on(True)
-    print(downloader.display_download_choices())
+    downloader.display_download_choices()
     downloader.set_video_choice(0)
     downloader.download()
